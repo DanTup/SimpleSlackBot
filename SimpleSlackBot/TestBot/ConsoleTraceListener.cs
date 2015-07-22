0603ee45f99a8a00ce3ba172553642a55aac0551
Danny Tuppeny <danny@tuppeny.com>
2015-07-22 20:58:14 +0100

    Added MainAsync and a trace listener for dumping communication to console.

---
+++ SimpleSlackBot/TestBot/ConsoleTraceListener.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Diagnostics;
+
+namespace TestBot
+{
+	class ConsoleTraceListener : TraceListener
+	{
+		public override void Write(string message)
+		{
+			Console.ForegroundColor = ConsoleColor.DarkGray;
+			Console.Write(message);
+			Console.ResetColor();
+		}
+
+		public override void WriteLine(string message)
+		{
+			Write(message);
+			Write("\r\n");
+		}
+	}
+}
--- SimpleSlackBot/TestBot/Program.cs
+++ SimpleSlackBot/TestBot/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -10,6 +11,21 @@ namespace TestBot
 	{
 		static void Main(string[] args)
 		{
+			Debug.Listeners.Add(new ConsoleTraceListener());
+
+			MainAsync(args).Wait();
+		}
+
+		static async Task MainAsync(string[] args)
+		{
+			// TODO: Connect bot...
+			Debug.WriteLine("Test...");
+
+			await Task.Delay(5000);
+
+			Console.WriteLine("Press a key to disconnect...");
+			Console.ReadKey();
+			Console.WriteLine();
 		}
 	}
 }
