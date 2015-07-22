337b209a448537a4da8815b71eb34e07d7491617
Danny Tuppeny <danny@tuppeny.com>
2015-07-22 20:51:20 +0100

    Empty class library.

---
+++ SimpleSlackBot/SimpleSlackBot/Properties/AssemblyInfo.cs
@@ -0,0 +1,7 @@
+﻿using System.Reflection;
+
+[assembly: AssemblyTitle("SimpleSlackBot")]
+[assembly: AssemblyProduct("SimpleSlackBot")]
+[assembly: AssemblyCopyright("Copyright Danny Tuppeny 2015")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
