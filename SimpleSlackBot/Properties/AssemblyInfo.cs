8a76de732e8ca0c125b20e135a44db4744ef59ed
Danny Tuppeny <danny@tuppeny.com>
2015-08-02 15:20:15 +0100

    Update version.

--- SimpleSlackBot/Properties/AssemblyInfo.cs
+++ SimpleSlackBot/Properties/AssemblyInfo.cs
@@ -5,5 +5,5 @@
 [assembly: AssemblyCompany("DanTup")]
 [assembly: AssemblyDescription("Simple bot library for Slack.")]
 [assembly: AssemblyCopyright("Copyright Danny Tuppeny 2015")]
-[assembly: AssemblyVersion("0.1.0.0")]
-[assembly: AssemblyFileVersion("0.1.0.0")]
+[assembly: AssemblyVersion("0.2.0.0")]
+[assembly: AssemblyFileVersion("0.2.0.0")]
